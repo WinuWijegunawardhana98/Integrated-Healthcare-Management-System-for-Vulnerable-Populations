64ee695456bae161b7e00f0d1077491df5ae0357
hackishmax321 <hackishmax321@gmail.com>
2025-02-22 16:11:35 +0530

    first commit

---
+++ web/src/data/Diets.js
@@ -0,0 +1,26 @@
+const dietItems = [
+  {
+    name: "Diabetic Salad",
+    imagePath: "https://www.eatingwell.com/thmb/bv6F3b9mi_a017XG_73h8TSVCoU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/7436258-68b68f1ecb954104affe85a0c8c1d85f.jpg",
+    nutritions: ["Protein", "Fiber", "Vitamins", "Minerals"],
+    suitableFor: ["Diabetes", "Heart Diseases"],
+  },
+  {
+    name: "Heart Healthy Oatmeal",
+    imagePath: "https://www.eatingwell.com/thmb/e2iV6HvZQa8lZyKiEX8lp7z_qR0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/EWL-251317-oatmeal-with-fruit-nuts-04-A-ad4552834cec480d8c6ea92b5f29aa40.jpg",
+    nutritions: ["Fiber", "Omega-3", "Protein"],
+    suitableFor: ["Heart Diseases", "Seniors"],
+  },
+  {
+    name: "Calcium-Rich Smoothie",
+    imagePath: "https://images.squarespace-cdn.com/content/v1/60f0a98861ccc05f5a850211/a0db671c-094e-4bf3-956d-c800cba6bb82/Smoothie.png",
+    nutritions: ["Calcium", "Vitamin D", "Fiber"],
+    suitableFor: ["Seniors"],
+  },
+  {
+    name: "Low-Sugar Green Juice",
+    imagePath: "https://images.squarespace-cdn.com/content/v1/61d58011a75874135ce99702/97f432f5-f348-49e9-819e-21343611e684/DA8184~1.JPG",
+    nutritions: ["Vitamins", "Fiber", "Antioxidants"],
+    suitableFor: ["Diabetes"],
+  },
+];
---
+++ web/src/pages/PatientsSection.js
@@ -0,0 +1,42 @@
+// pages/Dashboard.js
+import React, { useEffect, useState } from 'react';
+import Sidebar from '../components/Sidebar';
+import Topbar from '../components/Topbar';
+import Footer from '../components/Footer';
+import PatientManagement from '../components/PatientsManagement';
+import PatientSummary from '../components/PatientSummary';
+import AddPrescription from '../components/AddPrescriptions';
+import AddMedication from '../components/AddMedication';
+
+const PatientsSection = () => {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : {};
+  });
+  const [selectedUser, setSelectedUser] = useState(null); 
+
+  console.log(selectedUser)
+
+  useEffect(() => {
+    if (!user.username) {
+      console.warn("User data is not available or invalid.");
+    }
+  }, [user]);
+
+  const usernameToUse = selectedUser?.username || user.username;
+  return (
+    <div className="container">
+      <Sidebar />
+      <div className="main">
+        <Topbar />
+        <div className="details">
+          <PatientManagement setSelectedUser={setSelectedUser}/>
+          <AddMedication user={usernameToUse} />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default PatientsSection;
